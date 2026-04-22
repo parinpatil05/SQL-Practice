217aed3036ed57f073963071bbc8ef223e4cf60e
Parin Patil <parinvpatil@gmail.com>
2026-04-22 17:46:10 +0530

    Add SQL examples for INNER, LEFT, and RIGHT JOINs

--- joins.sql
+++ joins.sql
@@ -1,1 +1,17 @@
+-- INNER JOIN example
+SELECT students.name, courses.course_name
+FROM students
+INNER JOIN courses
+ON students.course_id = courses.id;
 
+-- LEFT JOIN example
+SELECT customers.name, orders.order_id
+FROM customers
+LEFT JOIN orders
+ON customers.id = orders.customer_id;
+
+-- RIGHT JOIN example
+SELECT employees.name, departments.department_name
+FROM employees
+RIGHT JOIN departments
+ON employees.department_id = departments.id;
