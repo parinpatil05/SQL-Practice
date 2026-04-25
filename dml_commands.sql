1dad629e95e45e305db708a243b21c12af9a4f23
Parin Patil <parinvpatil@gmail.com>
2026-04-25 22:09:04 +0530

    Insert complete customer record for Max
    Added an insert statement for a complete customer record.

--- dml_commands.sql
+++ dml_commands.sql
@@ -24,3 +24,8 @@ INSERT INTO customers (id, first_name, country, score)
 VALUES
 (6, 'Anna', 'USA', NULL),
 (7, 'Sam', NULL, 100);
+
+-- Insert a complete record
+INSERT INTO customers (id, first_name, country, score)
+VALUES (8, 'Max', 'USA', 368);
+
